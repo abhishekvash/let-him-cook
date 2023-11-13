6c933a1bb90ec59ee027f64892d4e4d428b4e93e
abhishekvash <abhishekvash34@gmail.com>
2023-11-13 17:33:58 +0530

    Add Home Link in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,17 @@
 import LogoNoBackgroundSVG from "@/assets/LogoNoBackground.svg";
+import { Link } from "react-router-dom";
 export function Navbar() {
 	return (
 		<>
 			<nav className="navbar sticky top-0 min-h-0 bg-primary/40 py-4 text-primary-content backdrop-blur">
 				<div className="flex-1">
-					<img
-						className="h-5"
-						src={LogoNoBackgroundSVG}
-						alt="Let Him Cook Logo"
-					/>
+					<Link to="/">
+						<img
+							className="h-5"
+							src={LogoNoBackgroundSVG}
+							alt="Let Him Cook Logo"
+						/>
+					</Link>
 				</div>
 			</nav>
 		</>
